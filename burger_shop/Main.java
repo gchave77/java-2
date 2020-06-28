91a8a5f0bf193f09510683033ae7fb4a2374b715
gabe chavez <gabe.chavez77@gmail.com>
2020-06-27 15:43:48 -1000

    completed HealthyBurger

--- burger_shop/Main.java
+++ burger_shop/Main.java
@@ -3,7 +3,7 @@ package burger_shop;
 public class Main {
 
     public static void main(String[] args) {
-        burger_shop.Burger burger = new burger_shop.Burger("Regular", "hamburger", 3.15, "White bun");
+        burger_shop.Burger burger = new burger_shop.Burger("Regular", "hamburger", 3.26, "white bun");
         System.out.println("Total price is " + burger.itemize());
         System.out.println(" ");
 
@@ -13,6 +13,12 @@ public class Main {
         burger.addTopping4("cheese", 1.25);
         System.out.println("Total price is " + burger.itemize());
         System.out.println(" ");
+
+        burger_shop.HealthyBurger healthyBurger = new
+                burger_shop.HealthyBurger("turkey", 4.26);
+        healthyBurger.healthyTopping1("egg", 2.50);
+        healthyBurger.healthyTopping2("avocado", 1.50);
+        System.out.println("Total Healthy burger price is " + healthyBurger.itemize());
 
     }
 
