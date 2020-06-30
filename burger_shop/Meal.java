480ac0499f1d469959e9d6d8eaf85b87971d81c0
gabe chavez <gabe.chavez77@gmail.com>
2020-06-29 23:15:45 -1000

    consolidate exercise packages

--- burger_shop/Meal.java
+++ burger_shop/Meal.java
@@ -1,4 +1,33 @@
 package burger_shop;
 
-public class Meal {
+public class Meal extends burger_shop.Burger {
+    private String[] drink = { "water", "gatorade", "coke", "sprite", "dr. pepper", "sweet tea", "diet coke" };
+    private double drinkPrice = 2.51;
+    private String[] side = { "fries", "chips", "apple pie", "ice cream", "fried pickles" };
+    private double sidePrice = 2.51;
+
+    public Meal(String name, String meat, double price, String bread, String[] drink, double drinkPrice, String[] side,
+            double sidePrice) {
+        super(name, meat, price, bread);
+        this.drink = drink;
+        this.drinkPrice = drinkPrice;
+        this.side = side;
+        this.sidePrice = sidePrice;
+    }
+
+    public String[] getDrink() {
+        return drink;
+    }
+
+    public double getDrinkPrice() {
+        return drinkPrice;
+    }
+
+    public String[] getSide() {
+        return side;
+    }
+
+    public double getSidePrice() {
+        return sidePrice;
+    }
 }
